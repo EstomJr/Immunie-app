1fe0b87662ea4d70d9531526ecca50e57dff0b70
EstomJr <estonjr2@hotmail.com>
2024-07-10 01:23:58 -0300

    feat: config for PWA

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+const APP_NAME = "Carteirinha Immunie";
+const APP_DESCRIPTION = "Carteirinha Immunie";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="description" content={APP_DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon2.ico" />
+      </Head>
+        <Component {...pageProps} />
+    </>
+  );
+}
